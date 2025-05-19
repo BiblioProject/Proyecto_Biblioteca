66c86e27cd84b4c8bd5fc8a830fe19183777ac3f
fati868 <dqh927401>
2025-05-19 06:06:17 +0000

    creación de páginas para editoriales, idiomas y géneros con tablas, paginación y eliminar(desactivar)

--- app_biblioteca/admin.py
+++ app_biblioteca/admin.py
@@ -2,33 +2,33 @@ from django.contrib import admin
 from .models import Genre, Editorial, Language, Book, Reader, Lending
 
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name','is_active')
     
 admin.site.register(Genre, GenreAdmin)
 
 class EditorialAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name','is_active')
     
 admin.site.register(Editorial, EditorialAdmin)
 
 class LanguageAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name','is_active')
     
 admin.site.register(Language, LanguageAdmin)
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title','author','publication_year','stock','available','is_active')
     
 admin.site.register(Book, BookAdmin)
 
 class ReaderAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name','first_surname','sanctions','sanction_amount','is_active')
     readonly_fields = ('sanctions', 'sanction_amount')
     
 admin.site.register(Reader, ReaderAdmin)
 
 class LendingAdmin(admin.ModelAdmin):
-    list_display = ('reader','book')
+    list_display = ('reader','book','date','state','is_active')
     readonly_fields=('estimated_return_date','state')
     
 admin.site.register(Lending, LendingAdmin)
--- app_biblioteca/views.py
+++ app_biblioteca/views.py
@@ -1,31 +1,67 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
 from django.template.response import TemplateResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib import auth
-from django.contrib import messages
-from django.shortcuts import redirect
-from .models import Lending, Book, Reader
+from django.contrib import auth, messages
+from .models import Lending, Book, Reader, Editorial, Genre, Language
+from django.conf import settings
+import glob,os, json
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 
 @login_required(login_url='/app_biblioteca/login/')
 def main(request):
-   lendings=Lending.objects.all()
-   for lending in lendings:
-      print(lending.estimated_return_date)
-   return render(request,'lendings.html',{'lendings':lendings})
+   lendings=Lending.objects.filter(is_active=True)
+   paginator = Paginator(lendings, 7)  # 7 elementos por página
+   page_number = request.GET.get("page")
+   page_obj = paginator.get_page(page_number)
+
+   return render(request, 'lendings.html', {'page_obj': page_obj})  # Pasamos page_obj correctamente
 
 @login_required(login_url='/app_biblioteca/login/')
 def books_view(request):
-   books = Book.objects.all()
-   for book in books:
-      print(book.title)
-   return render(request, 'books.html', {'books': books})
+   books = Book.objects.filter(is_active=True)
+   paginator = Paginator(books, 5)  # 5 elementos por página
+   page_number = request.GET.get("page")
+   page_obj = paginator.get_page(page_number)
+
+   return render(request, 'books.html', {'page_obj': page_obj})  # Pasamos page_obj correctamente
 
 @login_required(login_url='/app_biblioteca/login/')
 def readers_view(request):
-   readers = Reader.objects.all()
-   for reader in readers:
-      print(reader.name)
-   return render(request, 'readers.html', {'readers': readers})
+   readers = Reader.objects.filter(is_active=True)
+   paginator = Paginator(readers, 7)  # 7 elementos por página
+   page_number = request.GET.get("page")
+   page_obj = paginator.get_page(page_number)
+
+   return render(request, 'readers.html', {'page_obj': page_obj})  # Pasamos page_obj correctamente
+
+@login_required(login_url='/app_biblioteca/login/')
+def editorials_view(request):
+   editorials = Editorial.objects.filter(is_active=True)
+   paginator = Paginator(editorials, 7)  # 7 elementos por página
+   page_number = request.GET.get("page")
+   page_obj = paginator.get_page(page_number)
+
+   return render(request, 'editorials.html', {'page_obj': page_obj})  # Pasamos page_obj correctamente
+
+@login_required(login_url='/app_biblioteca/login/')
+def genres_view(request):
+   genres = Genre.objects.filter(is_active=True)
+   paginator = Paginator(genres, 7)  # 7 elementos por página
+   page_number = request.GET.get("page")
+   page_obj = paginator.get_page(page_number)
+
+   return render(request, 'genres.html', {'page_obj': page_obj})  # Pasamos page_obj correctamente
+
+@login_required(login_url='/app_biblioteca/login/')
+def languages_view(request):
+   language = Language.objects.filter(is_active=True)
+   paginator = Paginator(language, 7)  # 7 elementos por página
+   page_number = request.GET.get("page")
+   page_obj = paginator.get_page(page_number)
+
+   return render(request, 'languages.html', {'page_obj': page_obj})  # Pasamos page_obj correctamente
 
 def login(request):
    args = {}
@@ -41,3 +77,81 @@ def valida_user(request):
      else:
         messages.info(request, 'Credenciales incorrectas')
   return redirect('/app_biblioteca/')
+
+@login_required(login_url='/app_biblioteca/login/')
+def delete_lending(request):
+    if request.method == "POST":
+        lendingid = request.POST.get("lendingid", None)
+        if lendingid:
+            lending = get_object_or_404(Lending, id=lendingid)
+            lending.is_active = False
+            lending.save()
+            return JsonResponse({"success": True})
+        return JsonResponse({"success": False, "error": "No se proporcionó un ID válido"})
+    
+    return JsonResponse({"success": False, "error": "Método no permitido"})
+
+@login_required(login_url='/app_biblioteca/login/')
+def delete_reader(request):
+    if request.method == "POST":
+        readerid = request.POST.get("readerid", None)
+        if readerid:
+            reader = get_object_or_404(Reader, id=readerid)
+            reader.is_active = False
+            reader.save()
+            return JsonResponse({"success": True})
+        return JsonResponse({"success": False, "error": "No se proporcionó un ID válido"})
+    
+    return JsonResponse({"success": False, "error": "Método no permitido"})
+
+@login_required(login_url='/app_biblioteca/login/')
+def delete_book(request):
+    if request.method == "POST":
+        bookid = request.POST.get("bookid", None)
+        if bookid:
+            book = get_object_or_404(Book, id=bookid)
+            book.is_active = False
+            book.save()
+            return JsonResponse({"success": True})
+        return JsonResponse({"success": False, "error": "No se proporcionó un ID válido"})
+    
+    return JsonResponse({"success": False, "error": "Método no permitido"})
+
+@login_required(login_url='/app_biblioteca/login/')
+def delete_editorial(request):
+    if request.method == "POST":
+        editorialid = request.POST.get("editorialid", None)
+        if editorialid:
+            editorial = get_object_or_404(Editorial, id=editorialid)
+            editorial.is_active = False
+            editorial.save()
+            return JsonResponse({"success": True})
+        return JsonResponse({"success": False, "error": "No se proporcionó un ID válido"})
+    
+    return JsonResponse({"success": False, "error": "Método no permitido"})
+
+@login_required(login_url='/app_biblioteca/login/')
+def delete_genre(request):
+    if request.method == "POST":
+        genreid = request.POST.get("genreid", None)
+        if genreid:
+            genre = get_object_or_404(Genre, id=genreid)
+            genre.is_active = False
+            genre.save()
+            return JsonResponse({"success": True})
+        return JsonResponse({"success": False, "error": "No se proporcionó un ID válido"})
+    
+    return JsonResponse({"success": False, "error": "Método no permitido"})
+
+@login_required(login_url='/app_biblioteca/login/')
+def delete_language(request):
+    if request.method == "POST":
+        languageid = request.POST.get("languageid", None)
+        if languageid:
+            language = get_object_or_404(Language, id=languageid)
+            language.is_active = False
+            language.save()
+            return JsonResponse({"success": True})
+        return JsonResponse({"success": False, "error": "No se proporcionó un ID válido"})
+    
+    return JsonResponse({"success": False, "error": "Método no permitido"})
